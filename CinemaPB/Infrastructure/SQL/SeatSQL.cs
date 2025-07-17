6c63f01d907391f285feddb09264d9fb8aaba6c6
giecopags <giecoaceo.pagaling@dmc.edu.ph>
2025-07-17 15:41:21 +0800

    Finisehd functionalities for SeatLayoutForm1 and 2

---
+++ CinemaPB/Infrastructure/SQL/SeatSQL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaPB.Infrastructure.SQL
+{
+    public class SeatSQL
+    {
+        public const string GetSeatsForHall = @"SELECT SeatID, HallID, SeatNumber, IsAvailable
+                                                 FROM dbo.Seats
+                                                 WHERE HallID = @HallID";
+
+        public const string UpdateSeatAvailability = @"UPDATE dbo.Seats
+                                                       SET IsAvailable = @IsAvailable
+                                                       WHERE SeatNumber = @SeatNumber AND HallID = @HallID";
+    }
+}
